d03e78e8801b51b5b6835c1da7af854455bffbda
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-05-26 07:39:17 +0300

    hw-ts-1

---
+++ src/basik/3.ts
@@ -0,0 +1,11 @@
+type mixedType = string | number;
+let unionType: mixedType;
+unionType = "test";
+unionType = 7;
+
+
+type EnableOrDisable = 'enable' | 'disable';
+let literalType: EnableOrDisable;
+literalType = "enable";
+literalType = "disable"
+
